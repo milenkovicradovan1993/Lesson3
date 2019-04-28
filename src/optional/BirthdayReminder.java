bcece4cbf9c0688bd163cfb484f1f2bb0bc544fe
Radovan <Radovan1993@live.com>
2019-04-28 15:43:33 +0200

    BirthdayReminder change

--- src/optional/BirthdayReminder.java
+++ src/optional/BirthdayReminder.java
@@ -1,15 +1,32 @@
 package optional;
 
+import java.util.Scanner;
+
 public class BirthdayReminder {
 
 	public static void main(String[] args) {
 		
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "May 28th";
+		String dadsBirthday = "May 23rd";
+		String myBirthday = "January 16th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
+		Scanner input = new Scanner (System.in);
+		System.out.println("Which birthday do you want to know family");
+		String answer = input.nextLine();
+		if (answer.contains("mom")) {
+			System.out.println(answer + " - her birthday is: ");
+			System.out.println(momsBirthday);
+		} else if (answer.contains("dad")) {
+			System.out.println(answer + " - his birthday is: ");
+			System.out.println(dadsBirthday);
+		} else if (answer.contains("Radovan")) {
+			System.out.println(answer + " - your birthday is: ");
+			System.out.println(myBirthday);
+		} else {
+			System.out.println("Sorry, i don't remember that person's birthday!");
+		}
 	
 		// 3. Print out what the user typed
 		
